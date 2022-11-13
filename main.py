4368314de7de5e07f98d5f44785b7e7616717ca6
Propedeutico-ne <118089014+Propedeutico-nektagt@users.noreply.github.com>
2022-11-13 07:41:44 +0000

    a bit of work

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello")
+import os
+
+for (dirpath, dirnames, filenames) in os.walk('media/'):
+  print(dirpath)
+  for re_dirpath in dirnames:
+    print(f'\t{re_dirpath}')
+  for re_filespath in filenames:
+    print(f'\t{re_filespath}')
+  print()
