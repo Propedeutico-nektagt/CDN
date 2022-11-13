95be313fcc7c528dd7d603444983c24a7c2a35c2
Propedeutico-ne <118089014+Propedeutico-nektagt@users.noreply.github.com>
2022-11-13 20:39:12 +0000

    re template

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
+import os.path
 
+## Global variables ##
+URL = 'https://propedeutico-nektagt.github.io/CDN/'
+
+
+#print(os.path.dirname(__file__))
+
+## Jinja template ##
+env = Environment(
+  loader = FileSystemLoader("./templates/"),
+  autoescape = select_autoescape()
+)
+template = env.get_template("template.html")
+
+#print(template.render())
+
+## Main thread ##
 for (dirpath, dirnames, filenames) in os.walk('media/'):
-  print(dirpath)
+  print(f"{dirpath}\n")
   for re_dirpath in dirnames:
     print(f'\t{re_dirpath}')
   for re_filespath in filenames:
-    print(f'\t{re_filespath}')
+    print(f'{URL}{dirpath}/{re_filespath}')
   print()
