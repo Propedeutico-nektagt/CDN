5e8f38021a9aa7199ef4771712d4d2259959d8c3
Propedeutico-ne <118089014+Propedeutico-nektagt@users.noreply.github.com>
2022-11-13 21:37:41 +0000

    works?

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import os.path
 
 ## Global variables ##
 URL = 'https://propedeutico-nektagt.github.io/CDN/'
-
-
-#print(os.path.dirname(__file__))
+CWD = os.getcwd()
+STYLE = os.path.join(CWD,'templates','style.css')
 
 ## Jinja template ##
 env = Environment(
@@ -15,13 +14,12 @@ env = Environment(
 )
 template = env.get_template("template.html")
 
-#print(template.render())
-
 ## Main thread ##
 for (dirpath, dirnames, filenames) in os.walk('media/'):
-  print(f"{dirpath}\n")
-  for re_dirpath in dirnames:
-    print(f'\t{re_dirpath}')
-  for re_filespath in filenames:
-    print(f'{URL}{dirpath}/{re_filespath}')
-  print()
+  path = os.path.join(CWD,dirpath)
+  os.chdir(path)
+  with open('index.html', 'w') as html_f:
+    html_f.write(template.render(
+      style = STYLE,
+      dirnames = dirnames
+    ))
